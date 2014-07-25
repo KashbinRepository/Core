fb3ff2378523a3ff2eef89182f69ea57bd28b641
bmaharjan-micros <bmaharjan@micros.com>
2014-07-25 08:41:18 -0400

    Build Script Checkin

---
+++ Build/build.bat
@@ -0,0 +1,23 @@
+
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" "D:\Repo\Core\SRC\DNN_Platform_07.02.00_Install\DesktopModules\Samarpan\Samarpan.Modules.sln" /p:Configuration=Release
+
+pause
+
+
+
+/*
+
+rd /s d:\Repo
+mkdir d:\Repo
+cd d:\Repo
+d:
+
+
+git clone "https://kashbin:kashbin353@github.com/KashbinRepository/Core
+
+
+
+// get from git
+//msbuild  Desktop modules in release mode
+//Package Adkim/skins/loadscript, bin, Desktop Modules, js, portal/default/Skins & Containers
+*/
